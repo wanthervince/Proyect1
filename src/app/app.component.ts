1a9c3311b7e00397754679e36423fbb04bfc8ccc
WanTher-VinCe <lonely.wolf.1594@gmail.com>
2021-09-04 10:17:08 -0500

    push github

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'proyect1';
+  listAlumnos: any[] = [
+    { Nombre: 'Vince', Estado: 'Inactivo' },
+    { Nombre: 'Erick', Estado: 'Stanby' },
+    { Nombre: 'Triana', Estado: 'Retirado' },
+    { Nombre: 'Wanther', Estado: 'Activo' },
+  ];
+  mostrar = true;
+  Ocultar(): void {
+    this.mostrar = !this.mostrar;
+    console.log(this.mostrar);
+  }
 }
